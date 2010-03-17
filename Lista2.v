dd16232330c553d8f32bc03389ea758f8ac04576
Maciej Kotowicz <kotowicz.maciej@gmail.com>
2010-03-17 11:48:49 +0100

    list2: almoust finished

--- Lista2.v
+++ Lista2.v
@@ -1,17 +1,41 @@
 
+Section Zad1.
+
+  Inductive empty : Set :=
+    | e : empty -> empty.
+
+  Lemma empty_false: forall x : empty, False.
+  Proof.
+    intro.
+    induction x.
+    assumption.
+  Qed.
+
+Section Zad2.
+
+  Variable P Q R: Prop.
+
+  Definition F : P /\ (Q \/ R) -> (P /\ Q) \/ (P  /\ R)  :=
+    fun (A:P /\ (Q \/ R)) => and_ind (fun (p : P) (QR : Q \/ R) => 
+      or_ind (fun q : Q => or_introl (P /\ R) (conj p q))
+             (fun r : R => or_intror (P /\ Q) (conj p r))
+             QR) A.
+End Zad2.
+
+
 Section Zad3.
 
   Variable A : Type.
   
-  Definition num : (A -> A) -> A -> A  := fun(f:A->A) (x:A) => f x.
+  Definition num : Type :=  (A -> A) -> A -> A. 
  
-  Fixpoint nat2num (n:nat)  :=
+  Fixpoint nat2num (n:nat) : num :=
     match n with
-      | 0 => fun (f:A->A) (x:A) => x
-      | S n => fun (f:A->A) (x:A) => f (nat2num n f x)
+      | 0 => fun f x => x
+      | S n => fun f x => f (nat2num n f x)
     end.
 
-  Definition succ n := fun (f:A -> A ) (x:A) => f (n f x).
+  Definition succ (n : num ):  num := fun f x => f (n f x).
 
 
   Lemma succ_is_corrent : forall n:nat, succ (nat2num n) = nat2num(n+1).
@@ -28,7 +52,8 @@ Section Zad3.
     rewrite <- IHn.
     reflexivity.
   Qed.
-  Definition add (n m : (A -> A) -> A ->A ) := fun (f:A -> A) (x:A) => n f (m f x).
+
+  Definition add (n m : num )  : num := fun f x  => n f (m f x).
 
   Lemma add_is_correct : forall n m:nat, add (nat2num n) (nat2num m) = nat2num (n+m).
   Proof.
@@ -49,7 +74,7 @@ Section Zad3.
     reflexivity.
   Qed.
 
-  Definition mul (n m : (A -> A) -> A -> A) := fun (f:A -> A) (x:A) => n (m f ) x. 
+  Definition mul (n m : num ) : num := fun f x => n (m f ) x. 
 
   Lemma add_mul : forall n m : nat, mul (nat2num (S n)) (nat2num m) = add (nat2num m) (mul (nat2num n) (nat2num m)).
   Proof.
@@ -202,6 +227,28 @@ Section Zad5.
     reflexivity.
   Qed.
 
+  (* nie zbedne *)
+  Axiom fun_ext : forall A B (f g : A -> B), (forall x, f x = g x ) -> f = g.
+  
+  
+  Lemma eta_exp : forall A B (f : A -> B), f = fun x => f x.
+    intros.
+    apply fun_ext.
+    intros.
+    reflexivity.
+  Qed.
+  
+  Lemma  b_if :forall b:bool -> bbtree, (fun b0: bool => b b0) = (fun b0 : bool => if b0 then b true else b false).
+  Proof.
+    intros.
+    apply fun_ext.
+    intro.
+    destruct x.
+    reflexivity.
+    reflexivity.
+  Qed.
+
+  (*potrzebna ekstensjonalna rownosc funkcji, patrz askojomat wyzej*)
   Lemma conv1_conv2_id : forall b:bbtree, conv1(conv2 b) = b.
   Proof.
     intros.
@@ -209,8 +256,141 @@ Section Zad5.
     simpl.
     reflexivity.
     simpl.
-    rewrite H.
-    rewrite H.
-    replace b with (fun b0: bool => if b0 then b true else b false).
+    rewrite H;rewrite H.
+    replace b with (fun b0:bool => b b0).
+    rewrite <- b_if.
     reflexivity.
-    transitivity 
+    symmetry.
+    apply eta_exp.
+  Qed.
+
+End Zad5.
+
+Section Zad6.
+
+  Require Import List.
+  Open Scope list_scope.
+
+  Variable A : Type.
+
+  (* cos nie tak... *)
+  Inductive tree : Type :=
+  | leaf : A -> tree
+  | node : list tree  -> tree.
+
+  Fixpoint hight (t: tree) : nat :=
+    match t with
+      | leaf _ =>  0
+      | node xs => S (length xs)
+    end.
+
+  Lemma hight_leaf : forall (t:tree), hight t = 0 -> exists a:A, t = leaf a.
+  Proof.
+    intros.
+    induction t.
+    simpl.
+    exists a.
+    reflexivity.
+    induction l.
+    assert (hight (node nil) <> 0).
+    simpl.
+    intro.
+    inversion H0.
+    simpl in H;simpl in H0.
+    contradiction.
+    assert (hight (node (a::l)) <> 0).
+    simpl.
+    intro.
+    inversion H0.
+    simpl in H;simpl in H0.
+    contradiction.
+  Qed.
+
+  Lemma hight_node : forall (t:tree), (exists n:nat, hight t = 2 +n) -> exists l: list tree , t = node l /\ l <> nil. 
+  Proof.
+    intros.
+    induction t.
+    assert (forall n, hight (leaf a) <> S n).
+    intro.
+    simpl.
+    intro.
+    inversion H0.
+    simpl in H;simpl in H0.
+    elim H.
+    intros.
+    inversion H1.
+    exists l.
+    split.
+    reflexivity.
+    intro.
+    simpl in H.
+    elim H.
+    intro.
+    destruct l.
+    simpl.
+    intro.
+    inversion H1.
+    inversion H0.
+  Qed.
+    
+
+
+  Fixpoint getLabel (t : tree) : list A :=
+    match t with
+      | leaf a => cons a nil
+      | node xs => let fix concatMap (l:list tree) :=
+                     match l with
+                       | nil => nil
+                       | cons x xs => getLabel x ++ concatMap xs
+                     end
+                   in concatMap xs
+    end.
+
+  Lemma S_eq : forall n m,  S n  = S m ->  n = m.
+  Proof.
+    intros.
+    induction n.
+    induction m.
+    reflexivity.
+    discriminate.
+    injection H.
+    intro.
+    assumption.
+  Qed.
+
+  Lemma getLabel_non_empty : forall (n : nat ) (t: tree), (hight t = n )-> getLabel t <> nil.
+  Proof.
+    intros.
+    induction n.
+    assert (exists a:A, t = leaf a).
+    apply hight_leaf.
+    assumption.
+    elim H0.
+    intros.
+    rewrite  H1.
+    simpl.
+    intro.
+    inversion H2.
+    assert (exists l: list tree , t = node l /\ l <> nil).
+    apply  hight_node.
+    exists n.
+    induction t.
+    simpl in H;inversion H.
+    induction l.
+    simpl.
+    assert (1 <> 2).
+    intro.
+    inversion H0.
+    rewrite  S_eq.
+    discriminate.
+  
+
+  Qed
+
+End Zad6.
+    
+
+
+
+
+
