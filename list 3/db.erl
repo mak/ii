b3c89d794609c91309a8ef2bfbdfef46679524a8
Tomasz Maciejewski <ponton@jabster.pl>
2010-03-29 23:29:03 +0200

    list3: zadanie 3

---
+++ list 3/db.erl
@@ -0,0 +1,55 @@
+-module(db).
+-compile(export_all).
+
+-record(db, {type, data = []}).
+
+create(Record) -> {ok, #db{type = Record}}.
+
+
+insert(DB = #db{data = []}, Row) ->
+    {DB#db{data = [{1, Row}]}, 1};
+
+
+insert(DB = #db{data = [Rest = {Id, _} | _]}, Row) ->
+    {DB#db{data = [{Id + 1, Row}|Rest]}, Id + 1}.
+
+
+select(DB, Pred) -> 
+    try
+        {ok, lists:filter(fun({_, Row}) -> Pred(Row) end, DB#db.data)}
+    catch
+        _ -> {error, bad_query}
+    end.
+
+
+delete(DB, Pred) ->
+    try
+        DbPred = fun({_, Row}) -> Pred(Row) end,
+        {Deleted, Remained} = lists:partition(DbPred, DB#db.data),
+        {ok, DB#db{data = Remained}, Deleted}
+    catch
+        _ -> {error, wrong_query}
+    end.
+
+map_pred([], _, _) -> [];
+map_pred([L|LS], Pred, F) ->
+    case Pred(L) of
+        true -> [F(L)|map_pred(LS, Pred, F)];
+        false -> [L|map_pred(LS, Pred, F)]
+    end.
+
+update(DB, Pred, Func) ->
+    try
+        DbPred = fun({Id, Row}) -> 
+                    case Pred(Row) of
+                        true -> {Id, Func(Row)};
+                        false -> {Id, Row}
+                    end
+                 end,
+        Updated = lists:filter(fun({_, Row}) -> Pred(Row) end, 
+            DB#db.data),
+        NewData = lists:map(DbPred, DB#db.data),
+        {ok, DB#db{data = NewData}, Updated}
+    catch
+        _ -> {error, wrong_query}
+    end.
