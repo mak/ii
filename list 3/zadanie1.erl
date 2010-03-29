3258680f9d97337eddb6408e4a44bcae504184d5
Tomasz Maciejewski <ponton@jabster.pl>
2010-03-29 21:25:06 +0200

    icmp and udp printing

--- list 3/zadanie1.erl
+++ list 3/zadanie1.erl
@@ -3,16 +3,80 @@
 -module(zadanie1).
 -compile(export_all).
 
+-record(ip_header, {version = 4, header_length = 5, tos,
+                total_length = 576, identification, flags = 0, ttl, 
+                protocol, checksum, src, dst, additional, data}).
+
 main([]) ->
     loop().
 
-
 loop() -> 
-    Bytes = read_ip_version(),
-    io:format("Read: ~w\n", [Bytes]).
+    IP = read_ip_packet(),
+    print_ip_header(IP).
+
+ip_to_string(IP = [_, _, _, _]) ->
+    string:join(lists:map(fun erlang:integer_to_list/1, IP), ".").
+
+hex_digit(N) when (N >= 0) and (N =< 9) -> integer_to_list(N);
+hex_digit(10) -> "A";
+hex_digit(11) -> "B";
+hex_digit(12) -> "C";
+hex_digit(13) -> "D";
+hex_digit(14) -> "E";
+hex_digit(15) -> "F".
 
 
-read_ip_version() -> 
+byte_to_hex(N) when (N >= 0) and (N =< 255) ->
+    A = N div 16,
+    B = N rem 16,
+    hex_digit(A) ++ hex_digit(B).
+
+
+print_ip_header(IP) when is_record(IP, ip_header) ->
+    
+    Version = case (IP#ip_header.version) of
+            4 -> "IPv4";
+            6 -> "IPv6"
+        end,
+    
+    Dst = ip_to_string(IP#ip_header.dst),
+    Src = ip_to_string(IP#ip_header.src),
+
+    Protocol = case IP#ip_header.protocol of
+            tcp -> "TCP";
+            udp -> "UDP";
+            icmp -> "ICMP"
+       end,
+
+    PrettyIP = IP#ip_header{version = Version, src = Src, 
+                    protocol = Protocol, dst = Dst},
+
+    io:format("IP Header\n-------------------\n"
+               "Version: ~s\n"
+               "Header Length: ~w words\n"
+               "Type of Service: ~w\n"
+               "Total Length: ~w\n"
+               "Identification: ~w\n"
+               "Flags: ~w\n"
+               "TTL: ~w\n"
+               "Protocol: ~s\n"
+               "Checksum: ~w\n"
+               "Src: ~s\n"
+               "Dst: ~s\n"
+               "Additional: ~w\n~i\n",
+               tl(tuple_to_list(PrettyIP))),
+
+    case IP#ip_header.protocol of
+        tcp -> print_tcp_packet(IP#ip_header.data);
+        udp -> print_udp_packet(IP#ip_header.data);
+        icmp-> print_icmp_packet(IP#ip_header.data);
+        raw_data -> string:join(lists:map(fun(X) -> byte_to_hex(X) end, 
+                    IP#ip_header.data), " ")
+
+    end.
+
+
+read_ip_packet() -> 
     [Byte] = read_bytes(1),
     <<Version:4, HeaderLength:4>> = <<Byte>>,
     [ToS] = read_bytes(1),
@@ -20,15 +84,72 @@ read_ip_version() ->
     Identification = bytes_to_integer(read_bytes(2)),
     Flags = bytes_to_integer(read_bytes(2)),
     [TTL] = read_bytes(1),
-    [Protocol] = read_bytes(1),
+    Protocol = case read_bytes(1) of
+        [1] -> icmp;
+        [6] -> tcp;
+        [17] -> udp;
+        true -> raw_data
+    end,
     Checksum = bytes_to_integer(read_bytes(2)),
     SrcIP = read_bytes(4),
     DestIP = read_bytes(4),
     Additional = read_bytes(4 * HeaderLength - 20),
-    Data = read_bytes(TotalLength - 4 * HeaderLength),
-    {Version, HeaderLength, ToS, TotalLength, Identification, Flags,
+    Data = case Protocol of 
+        tcp -> read_tcp_packet();
+        udp -> read_udp_packet();
+        icmp -> read_icmp_packet();
+        true -> read_bytes(TotalLength - 4 * HeaderLength)
+    end,
+        
+    {ip_header, Version, HeaderLength, ToS, TotalLength, Identification, Flags,
      TTL, Protocol, Checksum, SrcIP, DestIP, Additional, Data}.
-    
+   
+
+read_udp_packet() ->
+    SrcPort = bytes_to_integer(read_bytes(2)),
+    DstPort = bytes_to_integer(read_bytes(2)),
+    Length = bytes_to_integer(read_bytes(2)),
+    Checksum = bytes_to_integer(read_bytes(2)),
+    Data = read_bytes(Length - 8),
+    {SrcPort, DstPort, Length, Checksum, Data}.
+
+
+print_udp_packet({SrcPort, DstPort, Length, Checksum, Data}) ->
+
+    PrettyData =  string:join(lists:map(fun(X) -> byte_to_hex(X) end, 
+                              Data), " "),
+
+    io:format("UDP Header:\n--------------------\n"
+              "Source port: ~w\n"
+              "Destination port: ~w\n"
+              "Length: ~w\n"
+              "Checksum: ~w\n"
+              "\n~s\n",
+              [SrcPort, DstPort, Length, Checksum, PrettyData]).
+
+
+read_icmp_packet() -> 
+    [Type] = read_bytes(1),
+    [Code] = read_bytes(1),
+    Checksum = bytes_to_integer(read_bytes(2)),
+    ID = bytes_to_integer(read_bytes(2)),
+    Sequence = bytes_to_integer(read_bytes(2)),
+    {Type, Code, Checksum, ID, Sequence}.
+
+
+print_icmp_packet(Packet) ->
+    io:format("ICMP Header:\n------------------\n"
+              "Type: ~w\n"
+              "Code: ~w\n"
+              "Checksum: ~w\n"
+              "ID: ~w\n"
+              "Sequence: ~w\n",
+              tuple_to_list(Packet)).
+
+
+read_tcp_packet() -> {}.
+print_tcp_packet(_) -> {}.
+
 
 is_hex_digit(D) ->
     (D >= "0") and (D =< "9")
@@ -36,6 +157,7 @@ is_hex_digit(D) ->
     (D >= "a") and (D =< "f")
     or
     (D >= "A") and (D =< "F").
+
 
 hex_to_integer("a") -> 10;
 hex_to_integer("b") -> 11;
