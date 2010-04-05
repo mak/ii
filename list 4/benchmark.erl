cce7097121934d1094f49d28220b2ec955c4703c
Tomasz Maciejewski <ponton@jabster.pl>
2010-04-05 22:06:11 +0200

    supervisor process

--- list 4/benchmark.erl
+++ list 4/benchmark.erl
@@ -1,33 +1,45 @@
 -module(benchmark).
--export([start/0, loop/3]).
+-export([start/0, loop/4]).
 
 -import(statistics).
 -import(ring).
 
 start() ->
     Statistics = statistics:start(),
-    Ring = ring:start(self(), Statistics, 5),
-    
-    receive
-        {ring, created, _} ->
-            io:format("Ring created\n"),
-            {ok, spawn(benchmark, loop, [Statistics, Ring, 1])}
-    after
-        3000 ->
-            io:format("Ring timeout!\n"),
-            Ring ! stop,
-            Statistics ! stop,
-            {error, ring_timeout}
-    end.
+    {ok, spawn(benchmark, loop, [Statistics, self(), 1, 1])}.
 
-loop(Statistics, Ring, TokenID) ->
+loop(Statistics, Ring, TokenID, RingID) ->
     receive
-        {Steps, StopTime} -> 
-            Statistics ! {token, start, TokenID, Steps, now()},
+        {token, Steps, undefined} ->
+            Ring ! {TokenID, Steps, undefined},
+            loop(Statistics, Ring, TokenID + 1, RingID);
+
+        {token, Steps, Seconds} -> 
+            {MegaSec, Sec, Milisec} = now(),
+            StopTime = {MegaSec + ((Sec + Seconds) div 1000000),
+               (Sec + Seconds) rem 1000000, Milisec},
             Ring ! {TokenID, Steps, StopTime},
-            loop(Statistics, Ring, TokenID + 1);
+            loop(Statistics, Ring, TokenID + 1, RingID);
 
+        {start, Size} ->
+            NewRing = ring:start(RingID, self(), Statistics, Size),
+            receive 
+                {ring, created, RingID} ->
+                    io:format("Ring ~w created with size: ~w\n", [RingID, Size]),
+                    Ring ! stop,
+                    loop(Statistics, NewRing, TokenID, RingID + 1)
+            after
+                3000 -> 
+                    io:format("Ring ~w timeout!\n", [RingID]),
+                    NewRing ! stop,
+                    loop(Statistics, Ring, TokenID, RingID)
+            end;
+            
         stop ->
+            Statistics ! stop,
             Ring ! stop,
-            Statistics ! stop
+            receive
+                {ring, destroyed, DestroyedRingID} ->
+                    io:format("Ring ~w destroyed\n", [DestroyedRingID])
+            end
     end.
